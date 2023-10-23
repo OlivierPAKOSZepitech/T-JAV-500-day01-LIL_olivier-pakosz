3a9ab8913104b1597b8af6c0899e084875d86f0b
Olivier PAKOSZ <olivier.pakosz@epitech.eu>
2023-10-23 12:46:43 +0200

    correction

---
+++ ex_02/Ex02.java
@@ -0,0 +1,13 @@
+public class Ex02 {
+    public static void getAngryDog(int nbr) {
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < nbr; i++) {
+            result.append("woof");
+        }
+        result.append("\n");
+    System.out.println(result);
+    }
+    public static void main(String[] args) {
+        getAngryDog(3);
+    }
+}
